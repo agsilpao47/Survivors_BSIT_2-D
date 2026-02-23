31c7fda95dd2c32230459f04366da74ad0339fb6
AndroidSilpao <andreiantoniosilpao@gmail.com>
2026-02-23 22:15:52 +0800

    Andrei Silpao(Update)

--- app/Controllers/Books.php
+++ app/Controllers/Books.php
@@ -64,5 +64,41 @@ class Books extends BaseController
             return $this->response->setStatusCode(404)->setJSON(['error' => 'Book not found']);
         }
     }
+ /**
+     * Update book
+     */
+    public function update($id = null)
+    {
+        $validation = $this->validate([
+            'title' => 'required|max_length[20]',
+            'book_name' => 'required|max_length[100]',
+            'genre' => 'required|max_length[50]',
+            'date_publish' => 'required|integer|min_length[4]|max_length[4]'
+        ]);
 
+        if (!$validation) {
+            return $this->response->setJSON(['success' => false, 'message' => 'Validation failed']);
+        }
+
+        $data = [
+            'title' => $this->request->getPost('title'),
+            'book_name' => $this->request->getPost('book_name'),
+            'genre' => $this->request->getPost('genre'),
+            'date_publish' => $this->request->getPost('date_publish')
+        ];
+
+        $updated = $this->bookModel->update($id, $data);
+
+        if ($updated) {
+            return $this->response->setJSON([
+                'success' => true,
+                'message' => 'Book updated successfully.'
+            ]);
+        } else {
+            return $this->response->setJSON([
+                'success' => false,
+                'message' => 'Error updating book.'
+            ]);
+        }
+    }
 }
